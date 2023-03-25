acf12cc8eb90080ae80ec5dd6946aa2f4f1c170a
Mohammed Hashim <mohdkhashim07@gmail.com>
2023-03-25 12:22:14 +0530

    initial commit

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import logo from "../../assets/logo.svg";
+
+const Navbar = () => {
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={''} alt={"logo"} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
